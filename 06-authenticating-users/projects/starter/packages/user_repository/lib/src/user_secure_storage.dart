82dacf26979fc0e31121ea1eae65418ce9022017
Moaz Ahmed <justmeyo2025@gmail.com>
2023-01-20 10:56:21 +0200

    add: Flutter Secure Storage Capability for the "User Storage" [token,email,username].

--- 06-authenticating-users/projects/starter/packages/user_repository/lib/src/user_secure_storage.dart
+++ 06-authenticating-users/projects/starter/packages/user_repository/lib/src/user_secure_storage.dart
@@ -1,3 +1,68 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-// TODO: Create a secure Data Source.
+class UserSecureStorage {
+  static const String _tokenKey = 'wonder-words-token';
+  static const String _usernameKey = 'wonder-words-username';
+  static const String _emailKey = 'wonder-words-email';
+
+  UserSecureStorage(
+    FlutterSecureStorage? secureStorage,
+  ) : _secureStorage = secureStorage ?? const FlutterSecureStorage();
+
+  final FlutterSecureStorage _secureStorage;
+
+  Future<void> upsertUserInfo({
+    required String username,
+    required String email,
+    String? token,
+  }) =>
+      Future.wait(
+        [
+          //username
+          _secureStorage.write(
+            key: _usernameKey,
+            value: username,
+          ),
+          //email
+          _secureStorage.write(
+            key: _emailKey,
+            value: email,
+          ),
+          //token if not null
+          if (token != null)
+            _secureStorage.write(
+              key: _tokenKey,
+              value: token,
+            ),
+        ],
+      );
+
+  Future<void> deleteUserInfo() => Future.wait(
+        [
+          _secureStorage.delete(
+            key: _usernameKey,
+          ),
+          _secureStorage.delete(
+            key: _emailKey,
+          ),
+          _secureStorage.delete(
+            key: _tokenKey,
+          ),
+
+          //Check if there is any other key to be saved , if not , just use this
+          // _secureStorage.deleteAll();
+        ],
+      );
+
+  Future<void> readUsername() => _secureStorage.read(
+        key: _usernameKey,
+      );
+
+  Future<void> readUserEmail() => _secureStorage.read(
+        key: _emailKey,
+      );
+
+  Future<void> readUserToken() => _secureStorage.read(
+        key: _tokenKey,
+      );
+}
